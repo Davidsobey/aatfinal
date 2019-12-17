8e932a046c0b5f5d4492c57e2b9760b27f0d03b1
David <davidsobey@live.co.uk>
2019-12-17 19:52:19 +0200

    Made dre's changes

--- php/process-form.php
+++ php/process-form.php
@@ -1,17 +1,16 @@
 <?php
-if (isset($_REQUEST['firstname'],$_REQUEST['email'])) {
+if (isset($_REQUEST['1'],$_REQUEST['4'])) {
       
-    $firstname = $_REQUEST['firstname'];
-	$lastname = $_REQUEST['lastname'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
-    $message = $_REQUEST['message'];
-    $phone = $_REQUEST['phone'];
+    $firstname = $_REQUEST['1'];
+	$lastname = $_REQUEST['2'];
+    $email = $_REQUEST['4'];
+    $message = $_REQUEST['3'];
+    $phone = $_REQUEST['5'];
 
 
       
     // Set your email address where you want to receive emails. 
-    $to = 'info@aatautoassist.co.za';
+    $to = 'david@vortechs.co.za';
       
     $subject = 'AAT:  Client Request has been made';
     $headers = "From: ".$firstname." <".$email."> (".$phone.") \r\n";
